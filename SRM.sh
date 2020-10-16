d223175086b729ad6a487d032fb3730fa506854e
Revtan18 <yejik14@mail.ru>
2020-10-16 15:19:42 +0200

    New update srm.sh

--- SRM.sh
+++ SRM.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-srm="mv file1 ~/RECYCLE"
+f=${file}
+
+echo "Are you sure that you want to remove this file?(y/n)"
+read ANSWER
+if $ANSWER = n
+then
+break
+else
+mv  $f ~/RECYCLE"
 
